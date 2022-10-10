ed019687f8182a478e1a6e7633f9838827108c09
Sombra <murksombra@gmail.com>
2022-10-10 17:30:15 -0300

    add arg flow

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,18 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cloud_provider = &args[1];
+    let mut wordlist = String::new();
+
     println!("[@] MiMiKlOuDz - Intelligence gathering tool [@]");
     
-    if let Ok(lines) = read_lines("./aws.txt"){
+    match cloud_provider.as_str() {
+        "aws" => (wordlist = "aws.txt".to_string()),
+        "digitalocean" => (wordlist = "digitalocean.txt".to_string()),
+        "oracle" => (wordlist = "oracle.txt".to_string()),
+        _ => println!("{} not supported yet.", cloud_provider.as_str()),
+    }
+
+    if let Ok(lines) = read_lines(wordlist){
         for line in lines {
             if let Ok(path) = line {
                 println!("{}",path);
