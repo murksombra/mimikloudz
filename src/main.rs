bb9feb89edf8aa6065d784b653a4a8d06808e013
Sombra <murksombra@gmail.com>
2022-10-10 16:59:28 -0300

    add file read
    Signed-off-by: Sombra <murksombra@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+
+fn main()-> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let cloud_provider = &args[1];
+    println!("[@] MiMiKlOuDz - Intelligence gathering tool [@]");
+    
+    let mut file = File::open("aws.txt")?;
+    let mut paths = String::new();
+    file.read_to_string(&mut paths)?;
+
+    Ok(())
 }
+
